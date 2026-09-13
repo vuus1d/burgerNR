c8f51ff00baec836454923db2462a4af8abf2dda
honor <honornurasyl@gmail.com>
2026-09-13 18:25:35 +0500

    Improve builder demonstration

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,31 @@
 import burgerbuilder.Burger;
 import burgerbuilder.BurgerDirector;
+import burgerbuilder.SmashBurgerBuilder;
 
 public class Main {
 
     public static void main(String[] args) {
-        BurgerDirector director = new BurgerDirector();
 
-        Burger smashBurger = director.buildSmashBurger();
+        Burger customBurger = new SmashBurgerBuilder()
+                .setBun("Brioche")
+                .setPatty("Double Beef")
+                .setCheese("Pepper Jack")
+                .setSauce("Spicy Mayo")
+                .addVegetable("Pickles")
+                .addVegetable("Jalapeno")
+                .addExtra("Crispy Bacon")
+                .build();
+
+        BurgerDirector director = new BurgerDirector();
         Burger chickenBurger = director.buildChickenBurger();
 
         System.out.println("=== BURGER BUILDER DEMO ===");
         System.out.println();
-        System.out.println("Smash Burger:");
-        System.out.println(smashBurger);
+        System.out.println("Custom Smash Burger:");
+        System.out.println(customBurger);
 
         System.out.println();
-        System.out.println("Chicken Burger:");
+        System.out.println("Director's Chicken Burger:");
         System.out.println(chickenBurger);
     }
 }
